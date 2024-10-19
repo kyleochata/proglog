97fa0a1379ad95ded61651dd2d2a3ffa9dfc77c8
kyleochata <kyleochata@gmail.com>
2024-10-19 01:27:58 -0700

    new, Append created for segment struct

---
+++ internal/log/segment.go
@@ -0,0 +1,80 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	api "github.com/kyleochata/proglog/api/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+type segment struct {
+	store      *store
+	index      *index
+	baseOffset uint64 //relative offset for position within this segment
+	nextOffset uint64 //absolute offset for position within the entire log
+	config     Config
+}
+
+// newSegment() is called when the log needs to create a new segment when the current active segment reaches its max size.
+func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baseOffset,
+		config:     c,
+	}
+
+	var err error
+	storeFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".store")),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.store, err = newStore(storeFile); err != nil {
+		return nil, err
+	}
+
+	indexFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset)),
+		os.O_RDWR|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.index, err = newIndex(indexFile, c); err != nil {
+		return nil, err
+	}
+	if off, _, err := s.index.Read(-1); err != nil {
+		s.nextOffset = baseOffset
+	} else {
+		s.nextOffset = baseOffset + uint64(off) + 1
+	}
+	return s, nil
+}
+
+// Append() writes the record to the segment and returns the newly appended record's offset. Appends data to the store then to the index.
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, err
+	}
+	if err = s.index.Write(
+		//calc the relative offset of the next record within this segment.
+		uint32(s.nextOffset-uint64(s.baseOffset)),
+		pos,
+	); err != nil {
+		return 0, err
+	}
+	s.nextOffset++
+	return cur, nil
+}

be3dc9368eda05ad23c55cfe335aebc2a89905d5
kyleochata <kyleochata@gmail.com>
2024-10-24 13:08:30 -0700

    dicovery membership test ok

---
+++ internal/discovery/membership_test.go
@@ -0,0 +1,85 @@
+package discovery_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/serf/serf"
+	. "github.com/kyleochata/proglog/internal/discovery"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+)
+
+// test sets up a cluster with multiple servers and checks that the Membership returns all the servers that joined the membership and updates after a server leaves the cluster.
+func TestMembership(t *testing.T) {
+	m, handler := setupMember(t, nil)
+	m, _ = setupMember(t, m)
+	m, _ = setupMember(t, m)
+
+	//handler's joins and leaves channels has how many times each even happened and for what servers. Status: Alive, Leaving, Left, Failed
+	require.Eventually(t, func() bool {
+		return len(handler.joins) == 2 &&
+			len(m[0].Members()) == 3 &&
+			len(handler.leaves) == 0
+	}, 3*time.Second, 250*time.Millisecond)
+
+	require.NoError(t, m[2].Leave())
+
+	require.Eventually(t, func() bool {
+		return len(handler.joins) == 2 &&
+			len(m[0].Members()) == 3 &&
+			serf.StatusLeft == m[0].Members()[2].Status &&
+			len(handler.leaves) == 1
+	}, 3*time.Second, 250*time.Millisecond)
+
+	require.Equal(t, fmt.Sprintf("%d", 2), <-handler.leaves)
+}
+
+// setupMember sets up a new member under a free port and with the member's length as the node name (must be unique). Member's length also signals whether the member is the cluster's initial member or we have a cluster to join.
+func setupMember(t *testing.T, members []*Membership) ([]*Membership, *handler) {
+	id := len(members)
+	ports := dynaport.Get(1)
+	addr := fmt.Sprintf("%s:%d", "127.0.0.1", ports[0])
+	tags := map[string]string{
+		"rpc_addr": addr,
+	}
+	c := Config{
+		NodeName: fmt.Sprintf("%d", id),
+		BindAddr: addr,
+		Tags:     tags,
+	}
+	h := &handler{}
+	if len(members) == 0 {
+		h.joins = make(chan map[string]string, 3)
+		h.leaves = make(chan string, 3)
+	} else {
+		c.StartJoinAddrs = []string{members[0].BindAddr}
+	}
+	m, err := New(h, c)
+	require.NoError(t, err)
+	members = append(members, m)
+	return members, h
+}
+
+type handler struct {
+	joins  chan map[string]string
+	leaves chan string
+}
+
+func (h *handler) Join(id, addr string) error {
+	if h.joins != nil {
+		h.joins <- map[string]string{
+			"id":   id,
+			"addr": addr,
+		}
+	}
+	return nil
+}
+
+func (h *handler) Leave(id string) error {
+	if h.leaves != nil {
+		h.leaves <- id
+	}
+	return nil
+}
